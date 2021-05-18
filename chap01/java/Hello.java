77877e24db843ff17b7780bade6b8f2998c915f3
Lutz Hamel <lutz.hamel@gmail.com>
2021-05-18 09:23:29 -0400

    added chap01 to source tree

---
+++ chap01/java/Hello.java
@@ -0,0 +1,11 @@
+class Hello {
+
+    public void print_msg() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        Hello o = new Hello();
+	o.print_msg();
+    }
+}
